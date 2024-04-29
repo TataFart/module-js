c7ea80f090256640fdb62377d1a444771fb183a5
TataFart <tatafartukova@yandex.ru>
2024-04-29 08:51:05 +0300

    delete comments

--- lesson2.js
+++ lesson2.js
@@ -1,10 +1,10 @@
-// const product = prompt("Введите наименование товара");
-// console.log("Название продукта:" , product);
-// const category = prompt("Введите категорию товара");
-// console.log("Категория:" , category, ", тип данных:", typeof category)
-// const price =  prompt("Введите количество товара");
-// console.log( "Цена: ", price, ", тип данных:", typeof +price);
-// const amount =  prompt("Введите стоимость товара");
-// console.log("Количество: ", amount, ", тип данных:", typeof +amount);
+const product = prompt("Введите наименование товара");
+console.log("Название продукта:" , product);
+const category = prompt("Введите категорию товара");
+console.log("Категория:" , category, ", тип данных:", typeof category)
+const price =  prompt("Введите количество товара");
+console.log( "Цена: ", price, ", тип данных:", typeof +price);
+const amount =  prompt("Введите стоимость товара");
+console.log("Количество: ", amount, ", тип данных:", typeof +amount);
 
-// console.log(`На складе находится ${amount} единиц товара "${product}" на общую сумму ${price * amount} тугриков` );
+console.log(`На складе находится ${amount} единиц товара "${product}" на общую сумму ${price * amount} тугриков` );
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// 'use strict';
+'use strict';
 
-// const product = "Коварный лис";
-// console.log("Название продукта:" , product);
-// const category = "Настольные игры";
-// console.log("Категория:" ,category)
-// const price = 1200;
-// console.log("Цена: ", price);
-// const amount = 3;
-// console.log("Количество: ", amount);
+const product = "Коварный лис";
+console.log("Название продукта:" , product);
+const category = "Настольные игры";
+console.log("Категория:" ,category)
+const price = 1200;
+console.log("Цена: ", price);
+const amount = 3;
+console.log("Количество: ", amount);
 
-// console.log("Общая сумма: ", price * amount)
+console.log("Общая сумма: ", price * amount)
 
