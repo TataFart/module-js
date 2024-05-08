ad96939314105a5e7a5b0d4b52c6ee0d9cea0f0c
TataFart <tatafartukova@yandex.ru>
2024-05-08 09:41:45 +0300

    victore

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,25 +1,27 @@
-const income = +prompt("Введите ваш доход");
+let income = +prompt("Введите ваш доход");
 let rest = income;
-tax = 0;
+let tax = 0;
 
-if (rest > 50000) {
-    rest = income - 50000; 
-    
-    tax = tax + rest * 0.3;
-    rest = income - rest;
+
+if (income > 50000) {
+    rest = income - 50000;    
+    tax = tax + rest * 0.3;   
+    income = income - rest;
+    console.log(rest);
+    console.log(income);
     console.log(`Сумма налога на доход ${income} составила  ${tax}`);
     
 }
 
-if  (rest > 15000) {
-    rest = income - 15000;
-    tax= tax + rest * 0.2;
-    rest = income - rest;
+if  (income > 15000) {
+    rest =  income - 15000;    
+    tax= tax + rest * 0.2;   
+    rest = income - rest;   
     console.log(`Сумма налога на доход ${income} составила  ${tax}`);
   
 }
 
-if (rest > 0) {
+if (income > 0) {
     tax = tax + rest * 0.13;
     console.log(`Сумма налога на доход ${income} составила  ${tax}`);
 }
