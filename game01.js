2ec5f7bfd080d9cc249dc22e961f4bbad6452e21
TataFart <tatafartukova@yandex.ru>
2024-08-22 14:37:02 +0300

    bot guesnumber recurs

--- game01.js
+++ game01.js
@@ -4,35 +4,68 @@ let game = true;
 const secretNumber = Math.round(Math.random() * 100);
 
 // while (userNumber !== nul  && +userNumber!== secretNumber)
-while (game){ 
-    userNumber = prompt("Компьютер загадал число от 1 до 100. Попробуй угадать его");
+// while (game){ 
+//     userNumber = prompt("Компьютер загадал число от 1 до 100. Попробуй угадать его");
 
 
-    switch (true) {
-        case userNumber === null:
-            game = false;
-            break;
-        case isNaN(+userNumber):
-        case +userNumber < 1 :
-        case +userNumber > 100 :        
-            alert("Некорректный ввод");
-            break;        
+//     switch (true) {
+//         case userNumber === null:
+//             game = false;
+//             break;
+//         case isNaN(+userNumber):
+//         case +userNumber < 1 :
+//         case +userNumber > 100 :        
+//             alert("Некорректный ввод");
+//             break;        
 
-        case userNumber > secretNumber :
-            alert(`Загаданное число меньше, чем  ${userNumber}`);
-            break;
+//         case userNumber > secretNumber :
+//             alert(`Загаданное число меньше, чем  ${userNumber}`);
+//             break;
             
-        case userNumber < secretNumber :
-            alert(`Загаданное число больше, чем  ${userNumber}`);
-            break;
+//         case userNumber < secretNumber :
+//             alert(`Загаданное число больше, чем  ${userNumber}`);
+//             break;
 
-        default:
-            alert("В точку! Угаал!!!"); 
-            game = false
+//         default:
+//             alert("В точку! Угаал!!!"); 
+//             game = false
                   
-    }     
-}  
-console.log("конец");
+//     }     
+// }  
+// console.log("конец");
 
+const gamebot = () =>{
+    if (game === true) {
+        
 
+        userNumber = prompt("Компьютер загадал число от 1 до 100. Попробуй угадать его");
+
+            switch (true) {
+                case userNumber === null:
+                    game = false;
+                    break;
+                case isNaN(+userNumber):
+                case +userNumber < 1 :
+                case +userNumber > 100 :        
+                    alert("Некорректный ввод");
+                    break;        
+        
+                case userNumber > secretNumber :
+                    alert(`Загаданное число меньше, чем  ${userNumber}`);
+                    break;
+                    
+                case userNumber < secretNumber :
+                    alert(`Загаданное число больше, чем  ${userNumber}`);
+                    break;
+        
+                default:
+                    alert("В точку! Угаал!!!"); 
+                    game = false;                          
+            }     
+            gamebot();
+      
+    }    
+}
+
+gamebot();
 
