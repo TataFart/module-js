19d340936c721daeb3b528b2b0f6fbef58c8de81
TataFart <tatafartukova@yandex.ru>
2024-10-08 22:42:13 +0300

    its works

--- game.js
+++ game.js
@@ -2,34 +2,24 @@
 (() => {
   const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
 
-  const getCompFigure = () => {
-    const compFigure = Math.floor(Math.random() * ((FIGURES_RUS.length - 1) - 0 + 1) + 0); ;
-    return compFigure;
+  const getCompFigure = () => { 
+    return Math.floor(Math.random() * FIGURES_RUS.length); 
   };
-  const playAgain = (res, rej) => {
-    while (!confirm('Сыграем ещё?')) {
-      if (confirm(`Хотите выйти?`) && confirm('Вы уверены?')) {
-        return rej();
-      }
-    }
-    return res();
-  };
-
 
   const getUserFigure = () => {
-    const userInput = prompt(`${FIGURES_RUS[0]}, ${FIGURES_RUS[1]}, ${FIGURES_RUS[2]}?`);
+    const userInput = prompt(`${FIGURES_RUS.join(", ")}?`);
 
     if (userInput === null) {
-      playAgain(getUserFigure(), () => alert('Пока'));
-      return undefined;
-    }
-
+      if (confirm("Вы уререны, что хотите выйти?")){
+        alert("Bye");
+        return null;
+      }
+      return getUserFigure();
+    }  
+    
     const userFigure = userInput.toLowerCase().trim();
-    console.log(userFigure);
-
 
     if (FIGURES_RUS.includes(userFigure)) {
-      console.log(FIGURES_RUS.indexOf(userFigure));
       return FIGURES_RUS.indexOf(userFigure);
     } else {
       alert('Неверный ввод, попробуйте снова.');
@@ -47,8 +37,7 @@
       const results = `Ваша фигура - ${FIGURES_RUS[userFigure]}, компьютер показал - ${FIGURES_RUS[compFigure]}.`;
       if (userFigure === compFigure) {
         alert(`${results} Ничья!`);
-      }
-      if ((userFigure + 1) % 3 === compFigure) {
+      } else if ((userFigure + 1) % 3 === compFigure) {
         result.player += 1;
         alert(`${results} Победа за вами!`);
       } else {
@@ -57,18 +46,24 @@
       }
     };
 
+    const playAgain = (res, rej) => {
+      while (!confirm('Сыграем ещё?')) {
+        if (confirm(`Компьютер набрал: ${result.computer} Ваши очки: ${result.player}. Хотите выйти?`)) {
+          return rej();
+        }
+      }
+      return res();
+    };  
+
     function start() {
       const userFigure = getUserFigure();
 
-      if (userFigure !== undefined) {
-        const compFig = getCompFigure();
-
-        console.log(compFig);
+      if (userFigure !== null) {
+        const compFig = getCompFigure();       
         checkWinner(userFigure, compFig);
 
-
-        playAgain(start(), () => alert(`Компьютер набрал: ${result.computer} Ваши очки: ${result.player}.`));
-      }
+        playAgain(() => start(), () => alert(`Компьютер набрал: ${result.computer} Ваши очки: ${result.player}.`));
+      }       
     }
 
     return start;
